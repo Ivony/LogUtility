0e540643ca5e5bc49ec630d45bc7302fcc0a28de
Ivony <Ivony@live.com>
2014-07-23 18:58:22 +0800

    PipedLogger

---
+++ Ivony.Logs/LoggerWithSource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Logs
+{
+  internal class LoggerWithSource : Logger, ILogMetaProvider
+  {
+
+
+    public LoggerWithSource( Logger logger, LogSource source )
+    {
+      InnerLogger = logger;
+      Source = source;
+
+    }
+
+    public Logger InnerLogger { get; private set; }
+
+    public LogSource Source { get; private set; }
+
+
+
+
+    protected override void WriteLog( LogEntry entry )
+    {
+      InnerLogger.LogEntry( entry );
+    }
+
+    public LogMeta GetLogMeta( LogType type )
+    {
+      return new LogMeta()
+      {
+        Source = Source,
+        Type = type
+      };
+    }
+
+  }
+}
