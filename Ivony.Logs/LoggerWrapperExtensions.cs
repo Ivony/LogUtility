0399449151329914c8cd1a2489a03cfb961ec486
Ivony <Ivony@live.com>
2015-04-13 11:07:54 +0800

    LoggerWithFilter 避免创建无意义的包装，增补注释。

--- Ivony.Logs/LoggerWrapperExtensions.cs
+++ Ivony.Logs/LoggerWrapperExtensions.cs
@@ -18,36 +18,38 @@ namespace Ivony.Logs
     {
 
 
-      private LogFilter _filter;
-
       public LoggerWithFilter( Logger logger, LogFilter filter )
       {
         Contract.Assert( logger != null );
         Contract.Assert( filter != null );
 
-        _filter = filter;
+        Filter = filter;
 
         InnerLogger = logger;
       }
 
 
-      public Logger InnerLogger
-      {
-        get;
-        private set;
-      }
+      public Logger InnerLogger { get; private set; }
+
+      public LogFilter Filter { get; private set; }
+
 
 
       public override void LogEntry( LogEntry entry )
       {
-        if ( _filter.Writable( entry ) )
+        if ( Filter.Writable( entry ) )
           InnerLogger.LogEntry( entry );
       }
+
     }
 
 
-
-
+    /// <summary>
+    /// 创建一个新的日志记录器，只记录符合条件的日志
+    /// </summary>
+    /// <param name="logger">内部日志记录器，符合条件的日志将使用这个日志记录器记录</param>
+    /// <param name="filter">日志记录条件</param>
+    /// <returns>只记录符合条件的日志的日志记录器</returns>
     public static Logger WithFilter( this Logger logger, LogFilter filter )
     {
       if ( logger == null )
@@ -56,7 +58,13 @@ namespace Ivony.Logs
       if ( filter == null )
         throw new ArgumentNullException( "filter" );
 
-      return new LoggerWithFilter( logger, filter );
+
+      var loggerWithFilter = logger as LoggerWithFilter;
+      if ( loggerWithFilter != null )
+        return new LoggerWithFilter( loggerWithFilter.InnerLogger, loggerWithFilter.Filter & filter );
+
+      else
+        return new LoggerWithFilter( logger, filter );
     }
 
 
@@ -91,6 +99,12 @@ namespace Ivony.Logs
     }
 
 
+    /// <summary>
+    /// 创建一个日志记录器，对于所有的日志添加日志源信息
+    /// </summary>
+    /// <param name="logger">内部日志记录器，将使用这个日志记录器进行实际的日志记录</param>
+    /// <param name="source">要添加的日志源信息</param>
+    /// <returns>会自动添加日志源信息的日志记录器</returns>
     public static Logger WithSource( this Logger logger, LogSource source )
     {
       if ( logger == null )
